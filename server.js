513061fb76ca2f5aeafb7c20efc4d8ab750752d6
M4K4R <37383837+M4K4R@users.noreply.github.com>
2022-02-09 09:58:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
 
 var app = express();
+var multer = require("multer");
+var upload = multer({ dest: "uploads/" });
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
+  console.log(req.file);
+  let name = req.file.originalname;
+  let type = req.file.mimetype;
+  let size = req.file.size;
 
-
+  res.json({ name: name, type: type, size: size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
